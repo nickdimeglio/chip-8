8596e8302df17df614257a9c0eddb5e81dbae678
Nick DiMeglio <contact@nickdimeglio.com>
2020-10-17 18:05:28 -0400

    Initial commit

---
+++ chip8.py
@@ -0,0 +1,6 @@
+"""Class file for chip8"""
+
+
+class Chip8:
+    def __init__(self):
+        self.status = "Created"
---
+++ main.py
@@ -0,0 +1,5 @@
+from chip8 import Chip8
+
+
+# Set up render system and register input callbacks
+
